4d5d3080aacc19baa4f01b12f991a897ae5136bd
Mohammad Shajalal <khanranu81@gmail.com>
2024-11-24 14:24:45 +0600

    image add and page-2 complete and responsive done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-primary': '#1D183A',
+        'color-secondary': '#16A8AA',
+        'color-text': '#1D183A',
+        'color-boring': '#D9D9D9',
+        'color-ass': '#333333',
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '40px',
+        md: '50px',
+      }
+    }
   },
   plugins: [],
 }
